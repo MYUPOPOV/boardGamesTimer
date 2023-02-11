1d0c55dfc618a71f759a136a0fdd40c7fca15625
Maxim Popov <m.popov@bpcbt.com>
2023-02-12 02:29:09 +0300

    002 Time Counting Script

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,29 +1,85 @@
-const timeRemaining = 60*20;
-var secondsCounting;
+const main = document.querySelector('main');
+const startingPlayer = document.getElementById('starting_player_name');
+const startButton = document.getElementById('start_game');
 
+const timeRemaining = 60*10;
+const addingTime = 10;
+let secondsCounting;
 
-const body = document.querySelector('body')
-const timerPlayer_1 = document.querySelector('.player_1__timeRemaing')
+// Задаем объекты и массив игроков
+const player_1 = {
+  timeRemaining: 0,
+  timer: document.querySelector('.player_1__timeRemaining'),
+  activeTurn: false,
+};
+const player_2 = {
+  timeRemaining: 0,
+  timer: document.querySelector('.player_2__timeRemaining'),
+  activeTurn: false,
+};
+const player_3 = {
+  timeRemaining: 0,
+  timer: document.querySelector('.player_3__timeRemaining'),
+  activeTurn: false,
+};
+const players = [player_1,player_2,player_3];
 
-timerPlayer_1.textContent=timeRemaining;
-
-
-const startCountTime = (reducingTime) => {
+// Начинаем обратный отсчет времени
+const startCountTime = (item) => {
   secondsCounting = setInterval(() => {
-    reducingTime--;
-    changeShowingTime(reducingTime);
+    item.timeRemaining--;
+    changeShowingTime(item);
   }, 1000);
 };
 
-const changeShowingTime = (showingTime) => {
-  const min = Math.trunc(showingTime / 60) ;
-  const sec = showingTime % 60;
+// Отображаем каждую секунду
+const changeShowingTime = (item) => {
+  const min = Math.trunc(item.timeRemaining / 60) ;
+  const sec = item.timeRemaining % 60;
   const timeString = `${min} мин ${sec} сек` 
-  timerPlayer_1.textContent=timeString;
+  item.timer.textContent=timeString;
 };
 
+// Начинаем игру: фриз имени, запуск времени для первого игрока
+const startGame = () => {
+  startingPlayer.disabled = true;
+  startButton.disabled = true;
+  document.querySelector('.header__label').classList.add('unactive');
+  players.forEach( (item) => {
+    item.timeRemaining = timeRemaining;
+    changeShowingTime(item)
+    
+  })
+  players[2].timeRemaining = players[0].timeRemaining - addingTime;
+  players[2].activeTurn = true;
+  changeActivePlayer();
+}
 
-body.addEventListener('click', () => { clearInterval(secondsCounting)})
+// Меняем активный таймер
+const changeActivePlayer = () => {
+  clearInterval(secondsCounting)
+  players.forEach((item, index) => {
+    if (item.activeTurn) {
+      item.timeRemaining = item.timeRemaining + addingTime;
+      item.activeTurn = false;
+      // players[index + 1].activeTurn = true;
+      // startCountTime(players[index + 1])
 
-startCountTime(timeRemaining);
+      if (index + 1 <= players.length - 1) {
+        players[index + 1].activeTurn = true
+        clearInterval(secondsCounting)
+        startCountTime(players[index + 1])
+      } else {
+        players[0].activeTurn = true
+        clearInterval(secondsCounting)
+        startCountTime(players[0])
+      }
+
+    }
+  })
+}
+
+
+startButton.addEventListener('click', startGame)
+main.addEventListener('click', changeActivePlayer)
 
