b80b5212c952ed2b14f5ca83e3a3258588620df1
Maxim Popov <m.popov@bpcbt.com>
2023-02-13 01:12:03 +0300

    003 Layout changes and Added notification

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,12 +1,17 @@
 const main = document.querySelector('main');
-const startingPlayer = document.getElementById('starting_player_name');
+const header = document.querySelector('header');
+const playerNameInputs = document.querySelectorAll('.input');
 const startButton = document.getElementById('start_game');
 const selectTimeRemaining = document.getElementById('select_timeRemaining');
 const selectAddingTime = document.getElementById('select_addingTime');
+const popup = document.querySelector('.popup');
+
 
 let addingTime;
 let secondsCounting;
 let activeIndex;
+let isGameStarted = false;
+let isGamePaused = false;
 
 // Задаем объекты и массив игроков
 const player_1 = {
@@ -34,6 +39,7 @@ const startCountTime = (item) => {
   secondsCounting = setInterval(() => {
     if (item.timeRemaining <= 1) {
       clearInterval(secondsCounting)
+      showNitification('Время закончилось');
     }
     item.timeRemaining--;
     changeShowingTime(item);
@@ -49,6 +55,7 @@ const changeShowingTime = (item) => {
   item.timer.textContent=timeString;
 };
 
+// Сменить цвет имени и таймера
 const changeColorTime = (item) => {
   let color;
   if (item.timeRemaining >= 600) {
@@ -69,7 +76,7 @@ const changeActivePlayer = () => {
   clearInterval(secondsCounting)
   players.forEach((item, index) => {
     if (item.activeTurn) {
-      activeIndex = index
+      activeIndex = index;
     }
   });
   players[activeIndex].timeRemaining = players[activeIndex].timeRemaining + addingTime;
@@ -80,31 +87,77 @@ const changeActivePlayer = () => {
   if (activeIndex + 1 < players.length) {
     players[activeIndex + 1].activeTurn = true
     startCountTime(players[activeIndex + 1])
+    activeIndex++;
+    if (!popup.classList.contains('popup-active')) {
+      showNitification(`+ ${addingTime} сек`);
+    }
   } else {
     players[0].activeTurn = true
     startCountTime(players[0])
+    activeIndex = 0;
+    if (!popup.classList.contains('popup-active')) {
+      showNitification(`+ ${addingTime} сек`);
+    }
   }
 }
 
+// Меняем имена в input на label
+const switchInputToLabel = (item) => {
+  const input = item.card.querySelector('.player__input')
+  const label = item.card.querySelector('.player__label')
+  if (input.value.trim()) {
+    label.textContent = input.value.toUpperCase()
+  } else {
+    label.textContent = input.getAttribute("placeholder")
+  }
+  input.classList.toggle('hidden')
+  label.classList.toggle('hidden')
+}
+
+// Переключить паузу игры
+const togglePause = () => {
+  if (isGamePaused) {
+    isGamePaused = !isGamePaused;
+    startCountTime(players[activeIndex])
+    showNitification('Игра возобновлена');
+  } else {
+    isGamePaused = !isGamePaused;
+    clearInterval(secondsCounting)
+    showNitification('Игра остановлена');
+  }
+}
+
+// Показать оповещение
+const showNitification = (message) => {
+  popup.classList.add('popup-active')
+  popup.querySelector('.popup__message').textContent=message;
+  setTimeout(() => {
+    popup.classList.remove('popup-active')
+  }, 900)
+} 
+
 // Начинаем игру: фриз имени, запуск времени для первого игрока
 const startGame = () => {
-  startingPlayer.disabled = true;
+  showNitification('Игра началась');
+  playerNameInputs.disabled = true;
   startButton.disabled = true;
   selectTimeRemaining.disabled = true;
   selectAddingTime.disabled = true;
-  addingTime = ++selectAddingTime.value;
-
-  document.querySelector('.header__label').classList.add('unactive');
+  addingTime = selectAddingTime.value;
+  addingTime = ++addingTime - 1;
   players.forEach( (item) => {
     item.timeRemaining = selectTimeRemaining.value*60;
     changeShowingTime(item)
-    
+    switchInputToLabel(item)
   })
-  players[2].timeRemaining = players[0].timeRemaining - addingTime;
-  players[2].activeTurn = true;
+  players[players.length - 1].timeRemaining = players[players.length - 1].timeRemaining - addingTime;
+  players[players.length - 1].activeTurn = true;
   changeActivePlayer();
+  activeIndex = 0;
+  isGameStarted = true
 }
 
 startButton.addEventListener('click', startGame)
-main.addEventListener('dblclick', changeActivePlayer)
+main.addEventListener('dblclick', () => {if (isGameStarted && !isGamePaused) {changeActivePlayer()} })
+header.addEventListener('dblclick', () => {if (isGameStarted) {togglePause()} } )
 
